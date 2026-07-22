9c2ebe4666caf3cc92fe0ca09c88fd6a5ae09b62
elisaa98 <elisaa@epcad.org>
2026-07-22 12:08:02 -0600

    Created the Entity Framework Core DbContext closes #10

---
+++ src/FinancialTracker/FinancialTracker.Server/Data/FinancialTrackerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FinancialTracker.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancialTracker.Api.Data
+{
+    public class FinancialTrackerDbContext : DbContext
+    {
+        public FinancialTrackerDbContext(DbContextOptions<FinancialTrackerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+
+    }
+}

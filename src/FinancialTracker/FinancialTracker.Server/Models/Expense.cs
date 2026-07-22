942ba50cefae61569cca6225f1e9517fa48bccff
elisaa98 <elisaa@epcad.org>
2026-07-22 11:42:23 -0600

    Added Expense entity model clsoses #8

---
+++ src/FinancialTracker/FinancialTracker.Server/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿namespace FinancialTracker.Api.Models
+{
+    public class Expense
+    {
+        public Guid Id { get; set; }
+        public required string Merchant { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateSpent { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

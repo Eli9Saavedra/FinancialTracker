de51ce040dcd16e40327cbe15e4c335c4cb7f1e7
elisaa98 <elisaa@epcad.org>
2026-07-22 10:55:50 -0600

    Completed Income entity model closes #7

---
+++ src/FinancialTracker/FinancialTracker.Server/Models/Income.cs
@@ -0,0 +1,14 @@
+﻿namespace FinancialTracker.Api.Models
+{
+    public class Income
+    {
+        public Guid Id { get; set; }
+        public required string Source { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateReceived { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

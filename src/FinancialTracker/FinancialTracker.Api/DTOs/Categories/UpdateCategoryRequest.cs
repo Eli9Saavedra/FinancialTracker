29a758b3af12cbefb5b7fc3d6615102b38f6acfb
elisaa98 <elisaa@epcad.org>
2026-07-22 15:43:51 -0600

    Created the category DTOs closes #14

---
+++ src/FinancialTracker/FinancialTracker.Api/DTOs/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,11 @@
+﻿using FinancialTracker.Api.Enums;
+
+namespace FinancialTracker.Api.DTOs.Categories
+{
+    public class UpdateCategoryRequest
+    {
+        public required string Name { get; set; }
+        public CategoryType Type { get; set; }
+        public string? Description { get; set; }
+    }
+}

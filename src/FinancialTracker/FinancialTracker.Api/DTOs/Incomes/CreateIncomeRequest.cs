6bd6bccdea59b796d0187433c0fdbe837fe132bc
elisaa98 <elisaa@epcad.org>
2026-07-23 14:39:57 -0600

    Added income DTOs

---
+++ src/FinancialTracker/FinancialTracker.Api/DTOs/Incomes/CreateIncomeRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace FinancialTracker.Api.DTOs.Incomes
+{
+    public class CreateIncomeRequest
+    {
+        public required string Source { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateReceived { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string? Notes { get; set; }
+    }
+}

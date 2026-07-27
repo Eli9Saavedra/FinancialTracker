ab73625e28e5c409e703c7da633bc005e814f1e4
elisaa98 <elisaa@epcad.org>
2026-07-27 16:24:55 -0600

    Add category spending endpoint closes #38

--- src/FinancialTracker/FinancialTracker.Api/Controllers/SummariesController.cs
+++ src/FinancialTracker/FinancialTracker.Api/Controllers/SummariesController.cs
@@ -27,5 +27,18 @@ namespace FinancialTracker.Api.Controllers
 
             return Ok(result);
         }
+
+        [HttpGet("categories")]
+        public async Task<ActionResult<IEnumerable<CategorySpendingDto>>> GetCategorySpendingAsync([FromQuery] int month, [FromQuery] int year)
+        {
+            if (month < 1 || month > 12 || year < 1)
+            {
+                return BadRequest();
+            }
+
+            var result = await _summaryService.GetCategorySpendingAsync(month, year);
+
+            return Ok(result);
+        }
     }
 }
